15c17c211ce4f68a499737bd8951e51fc0b5b1fe
Maxime LUCE <somatic@live.fr>
2013-08-07 04:52:40 +0200

    Append Grunt configuration and Project Files

---
+++ Gruntfile.js
@@ -0,0 +1,61 @@
+﻿module.exports = function (grunt) {
+    // Parse Options from CLI
+    var outputDirectory = grunt.option("outputDirectory") || "build",
+        paths = {
+            output: outputDirectory,
+            base: outputDirectory + "/spa",
+            ui: outputDirectory + "/spa/ui"
+        };
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        paths: paths,
+
+        typescript: {
+            options: {
+                target: "es3",
+                module: "amd",
+                sourcemap: false,
+                declaration: true,
+                comments: false,
+                disallowbool: true,
+                disallowimportmodule: true
+            },
+            base: {
+                src: "base/*.ts",
+                dest: "<%= paths.output %>/spatools.js",
+                options: {
+                    module: "commonjs"
+                }
+            },
+            modules: {
+                src: "src/*.ts",
+                dest: "<%= paths.base %>/",
+                options: {
+                    declaration: false
+                }
+            }
+        },
+
+        uglify: {
+            base: {
+                src: "<%= typescript.base.dest %>",
+                dest: "<%= paths.output %>/spatools.min.js"
+            }
+        }
+    });
+
+    // Load plugins
+    grunt.loadNpmTasks('grunt-contrib');
+    grunt.loadNpmTasks('grunt-typescript');
+
+
+    // Build Steps
+    grunt.registerTask("build_base", ["typescript:base", "uglify:base"]);
+    grunt.registerTask("build_modules", ["typescript:modules"]);
+
+
+    // Buildset Tasks
+    grunt.registerTask("default", ["build_base", "build_modules"]);
+};
