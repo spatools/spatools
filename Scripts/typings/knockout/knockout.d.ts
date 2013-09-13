2784a0a3bd5d135a74552ee1f56544cc525a6d62
Maxime LUCE <somatic@live.fr>
2013-09-13 14:15:32 +0200

    Update knockout typing

--- Scripts/typings/knockout/knockout.d.ts
+++ Scripts/typings/knockout/knockout.d.ts
@@ -13,7 +13,7 @@ interface KnockoutInstancePrototypeStatic<T> extends KnockoutPrototypeStatic<T> 
 }
 
 interface KnockoutSubscribableFunctions<T> {
-    subscribe(callback: (newValue: T) => void , target?: any, topic?: string): KnockoutSubscription;
+    subscribe(callback: (newValue: T) => void , target?: any, topic?: string): KnockoutSubscription<T>;
     notifySubscribers(valueToWrite: T, topic?: string);
     getSubscriptionsCount(): number;
     extend(source);
@@ -218,6 +218,8 @@ interface KnockoutVirtualElements {
 }
 
 interface KnockoutExtenders {
+    [name: string]: any;
+
     throttle(target: any, timeout: number): KnockoutComputed<any>;
     notify(target: any, notifyWhen: string): any;
 }
@@ -332,8 +334,6 @@ interface KnockoutUtils {
     //setTextContent(element: any, textContent: string): void; // NOT PART OF THE MINIFIED API SURFACE (ONLY IN knockout-{version}.debug.js) https://github.com/SteveSanderson/knockout/issues/670
 
     setElementName(element: any, name: string): void;
-
-    ensureSelectElementIsRenderedCorrectly(selectElement);
 
     forceRefresh(node: any): void;
 
