5064b15823e954141a1fbdd336cdddad58f4d575
Maxime LUCE <somatic@live.fr>
2013-09-13 14:25:44 +0200

    Appen historyObservable to modules

---
+++ base/extenders.ts
@@ -0,0 +1,107 @@
+/// <reference path="../_references.d.ts" />
+/// <reference path="base.d.ts" />
+
+interface KnockoutExtenders {
+    moment: (target: any, options: Object) => any;
+    momentDuration: (target: any, options: any) => any;
+    delay: (target: any, delay: number) => any;
+    cnotify: (target: any, notifyWhen: any) => any;
+    cthrottle: (target: any, timeout: number) => any;
+    //notify: (target: any, notifyWhen: string, customEqualityComparer: (v1: any, v2: any) => number) => any;
+}
+
+ko.extenders.delay = function (target: any, delay: number): any {
+    var value = target();
+
+    target.timer = null;
+    target.immediate = ko.observable(value);
+
+    target.subscribe(target.immediate);
+    target.immediate.subscribe(function (newValue) {
+        if (newValue !== target()) {
+            if (target.timer) {
+                clearTimeout(target.timer);
+            }
+
+            target.timer = setTimeout(() => target(newValue), delay);
+        }
+    });
+
+    return target;
+};
+
+ko.extenders.cnotify = function (target: any, notifyWhen: any): any {
+    var latestValue: any = null,
+        superNotify: Function = _.bind(ko.subscribable.fn.notifySubscribers, target),
+        notify = function (value) {
+            superNotify(latestValue, "beforeChange");
+            superNotify(value);
+        };
+    
+    target.notifySubscribers = function (value, event) {
+        if (_.isFunction(notifyWhen)) { // custom
+            if (event === "beforeChange") {
+                latestValue = target.peek()
+                }
+            else if (!notifyWhen(latestValue, value)) {
+                notify(value);
+            }
+            return;
+        }
+
+        switch (notifyWhen) {
+            case "primitive":
+                if (event === "beforeChange") {
+                    latestValue = target.peek()
+                    }
+                else if (!ko.observable.fn.equalityComparer(latestValue, value)) {
+                    notify(value);
+                }
+                break;
+            case "reference":
+                if (event === "beforeChange") {
+                    latestValue = target.peek()
+                    }
+                else if (latestValue !== value) {
+                    notify(value);
+                }
+                break;
+            default:
+                //case "auto":
+                //case "always":
+                superNotify.apply(null, arguments);
+                break;
+        }
+    };
+
+    return target;
+};
+
+ko.extenders.notify = function (target: any, notifyWhen: any): any {
+    if (_.isFunction(notifyWhen)) { // custom
+        target.equalityComparer = notifyWhen;
+        return target;
+    }
+    switch (notifyWhen) {
+        case "always":
+            target.equalityComparer = () => false
+            break;
+        case "manual":
+            target.equalityComparer = () => true;
+            break;
+        case "reference":
+            target.equalityComparer = (a, b) => a === b;
+            break;
+        default:
+            //case "primitive":
+            target.equalityComparer = ko.observable.fn.equalityComparer;
+            break;
+    }
+
+    return target;
+};
+
+ko.extenders.cthrottle = function (target: any, timeout: number): any {
+    target['throttleEvaluation'] = timeout;
+    return target;
+};
---
+++ spa/historyObservable.ts
@@ -0,0 +1,89 @@
+﻿/// <reference path="_definitions.d.ts" />
+
+interface KnockoutHistoryObservableFunctions<T> {
+    back(): KnockoutHistoryObservable<T>;
+    next(): KnockoutHistoryObservable<T>;
+    replace(value: any): void;
+    reset(value?: any): void;
+}
+
+interface KnockoutHistoryObservable<T> extends KnockoutHistoryObservableFunctions<T> {
+    (): T;
+    (value: T): KnockoutHistoryObservable<T>;
+
+    latestValues: KnockoutObservableArray<T>;
+    selectedIndex: KnockoutObservable<number>;
+
+    canGoBack: KnockoutComputed<boolean>;
+    canGoForward: KnockoutComputed<boolean>;
+}
+
+var historyObservable: any = function historyObservable<T>(initialValue: T): KnockoutHistoryObservable<T> {
+    var self = {
+        latestValues: ko.observableArray([initialValue]),
+        selectedIndex: ko.observable(0)
+    };
+    $.extend(self, {
+        canGoBack: ko.computed(function () { return self.selectedIndex() > 0; }),
+        canGoNext: ko.computed(function () { return self.selectedIndex() < self.latestValues._size() - 1; }),
+    });
+    var result = ko.computed({
+        read: function () {
+            var index = self.selectedIndex(),
+                values = self.latestValues();
+
+            if (index > values.length)
+                index = 0;
+
+            return values[index];
+        },
+        write: function (value) {
+            var index = self.selectedIndex();
+            if (value !== self.latestValues()[index]) {
+                if (index !== self.latestValues._size() - 1) {
+                    self.latestValues.splice(index + 1);
+                }
+
+                self.latestValues.push(value);
+                self.selectedIndex(index + 1);
+            }
+        }
+    }).extend({ cnotify: "reference" });
+
+    $.extend(result, self);
+    $.extend(result, ko.historyObservable.fn);
+
+    return result;
+};
+historyObservable.fn = {
+    back: function () {
+        if (this.canGoBack()) {
+            this.selectedIndex(this.selectedIndex() - 1);
+        }
+        return this();
+    },
+    next: function () {
+        if (this.canGoNext()) {
+            this.selectedIndex(this.selectedIndex() + 1);
+        }
+        return this();
+    },
+    replace: function (value) {
+        var superNotify = _.bind(ko.subscribable.fn.notifySubscribers, this),
+            oldValue = this();
+
+        var index = this.selectedIndex();
+        this.latestValues.valueWillMutate();
+        this.latestValues()[index] = value;
+        this.latestValues.valueHasMutated();
+    },
+    reset: function (value) {
+        if (!value)
+            value = this();
+
+        this.latestValues.splice(0, this.latestValues._size(), value);
+        this.selectedIndex(0);
+    }
+};
+
+export = historyObservable;
