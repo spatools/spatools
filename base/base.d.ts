176653ca2d29f557829afe05edce23a68df796c5
SomaticIT <contact@somatic.fr>
2013-11-19 19:15:26 +0100

    Remove old base library

--- base/base.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference path="base.ts" />
-/// <reference path="extenders.ts" />
-/// <reference path="handlers.ts" />
-/// <reference path="knockout.ts" />
-/// <reference path="moment.ts" />
-/// <reference path="underscore.ts" />
